02de607046579d8af270ab42f978cd088c074779
dhwagdre <dheerajwag92@gmail.com>
2025-04-05 18:21:35 -0400

    added new line in JavaDemoTest

--- src/main/java/git/learnings/JavaDemoTest.java
+++ src/main/java/git/learnings/JavaDemoTest.java
@@ -4,6 +4,7 @@ public class JavaDemoTest {
     public static void main(String[] args) {
         System.out.println("Git first project");
         System.out.println("Git first project");
+        System.out.println("Added line 3");
     }
 
 }
