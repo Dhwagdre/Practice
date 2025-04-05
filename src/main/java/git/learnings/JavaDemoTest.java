d92bb4f7d30b279634141dec30431fce8c736b97
dhwagdre <dheerajwag92@gmail.com>
2025-04-05 18:06:16 -0400

    New project

---
+++ src/main/java/git/learnings/JavaDemoTest.java
@@ -0,0 +1,9 @@
+package git.learnings;
+
+public class JavaDemoTest {
+    public static void main(String[] args) {
+        System.out.println("Git first project");
+        System.out.println("Git first project");
+    }
+
+}
